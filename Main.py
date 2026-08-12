2a8cd6d96f41224451fe75e9d4120f57e72dbfc6
CrazyDogAlt <zanefranceschini63@gmail.com>
2026-08-12 14:05:41 +1000

    Add logging

--- Main.py
+++ Main.py
@@ -31,10 +31,12 @@ async def notrains(ctx, user: discord.User, reason: str=None):
         reason = "No reason provided."
     # Get No Train Role Object
     notrainrole = discord.utils.get(ctx.guild.roles, id=int(NO_TRAIN_ROLE))
+    modlogs = discord.utils.get(ctx.guild.channels, id=int(MOD_LOGGING_CHANNEL))
     user = await ctx.guild.fetch_member(user.id)
     # Attempt to apply role to user
     try:
         await user.add_roles(notrainrole)
+        modlogs.send(ctx.author.name + " removed " + user.name + "'s access from the train channel. Reason: " + reason)
     except:
         await ctx.send("Failed to give role to user, prehaps my role isn't high enough in the hierachy.")
 
@@ -56,9 +58,11 @@ async def yestrains(ctx, user: discord.User, reason: str=None):
     if reason == None:
             reason = "No reason provided."
     notrainrole = discord.utils.get(ctx.guild.roles, id=int(NO_TRAIN_ROLE))
+    modlogs = discord.utils.get(ctx.guild.channels, id=int(MOD_LOGGING_CHANNEL))
     user = await ctx.guild.fetch_member(user.id)
     try:
         await user.remove_roles(notrainrole)
+         modlogs.send(ctx.author.name + " reinstated " + user.name + "'s access from the train channel. Reason: " + reason)
     except:
         await ctx.send("Failed to remove role to user, prehaps my role isn't high enough in the hierachy.")
 
