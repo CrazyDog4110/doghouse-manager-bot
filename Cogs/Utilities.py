c399beb540ccb937427a1db8f034424706c52127
CrazyDog4110 <108052206+CrazyDog4110@users.noreply.github.com>
2026-09-22 17:22:59 +1000

    Add a slowmode command

--- Cogs/Utilities.py
+++ Cogs/Utilities.py
@@ -22,5 +22,19 @@ class Utilities(commands.Cog):
         await ctx.bot.close()
         quit()
 
+    @commands.command(aliases=["sm"])
+    @commands.has_permissions(manage_messages=True)
+    async def slowmode(self, ctx, duration, *, channel: discord.TextChannel = None):
+        if channel == None:
+            channel = ctx.channel
+
+        try:
+            duration = int(duration)
+        except:
+            await ctx.send("The slowmade duration has to be a number...")
+
+        await channel.edit(slowmode_delay=duration)
+        await ctx.send("Set slowmode in <#"+str(channel.id)+"> to "+str(duration)+" seconds")
+
 async def setup(bot):
     await bot.add_cog(Utilities(bot))
